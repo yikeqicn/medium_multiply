ef00429cc416d8fb3dffc58a8f94bd904af067ae
Yike Qi <Yike.Qi@ey.com>
2022-10-02 11:01:28 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#from medium_multiply.multiplication import Multiplication
+from medium_multiply import Multiplication
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+print(multiplication.multiply(5))
